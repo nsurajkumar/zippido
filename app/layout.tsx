71baf912ca5deb8327f240d00473c3ef6d594cb4
suraj <surajsunny024@gmail.com>
2025-11-03 00:52:14 +0530

    favicon also added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,44 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Zippido",
-  description: "Your GetAway Driver",
+    title: "Zippido",
+    description: "Your GetAway Driver",
+    icons: {
+        icon: [
+            {url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png'},
+            {url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png'},
+        ],
+        apple: [
+            {url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png'},
+        ],
+    },
+    manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
